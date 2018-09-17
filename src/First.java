f1522e19b5d1a89c28f62184bebf17478c5acf80
sudheerksr <sudheerksrgdk@gmail.com>
2018-09-17 10:36:14 +0530

    Changes in First

--- src/First.java
+++ src/First.java
@@ -52,6 +52,8 @@ public class First {
 		int n = 10, t1 = 0, t2 = 1;
 		for (int i = 1; i < n; i++) {
 			System.out.println(t1);
+				System.out.println(t1);
+					System.out.println(t1);
 			int sum = t1 + t2;
 			t1 = t2;
 			t2 = sum;
